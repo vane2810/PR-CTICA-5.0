c47ac5dd00cef94e8218a1f0f6e7f0eb298b4058
vane2810 <olgavanessasf@gmail.com>
2022-09-12 05:46:44 -0600

    EJERCICIO 1 - CALCULADORA

---
+++ PRÁCTICA 5.2/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace PRÁCTICA_5._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.comboOperaciones = new System.Windows.Forms.ComboBox();
+            this.txt_num1 = new System.Windows.Forms.TextBox();
+            this.txt_num2 = new System.Windows.Forms.TextBox();
+            this.txt_num3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(41, 63);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 0;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(201, 63);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 20);
+            this.txtNum2.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.Window;
+            this.btnCalcular.Location = new System.Drawing.Point(209, 133);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(86, 31);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // comboOperaciones
+            // 
+            this.comboOperaciones.FormattingEnabled = true;
+            this.comboOperaciones.Items.AddRange(new object[] {
+            "Suma",
+            "Resta",
+            "Multiplicación",
+            "División"});
+            this.comboOperaciones.Location = new System.Drawing.Point(31, 139);
+            this.comboOperaciones.Name = "comboOperaciones";
+            this.comboOperaciones.Size = new System.Drawing.Size(121, 21);
+            this.comboOperaciones.TabIndex = 3;
+            // 
+            // txt_num1
+            // 
+            this.txt_num1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txt_num1.Location = new System.Drawing.Point(65, 37);
+            this.txt_num1.Name = "txt_num1";
+            this.txt_num1.Size = new System.Drawing.Size(57, 20);
+            this.txt_num1.TabIndex = 4;
+            this.txt_num1.Text = "Número 1";
+            // 
+            // txt_num2
+            // 
+            this.txt_num2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txt_num2.Location = new System.Drawing.Point(222, 37);
+            this.txt_num2.Name = "txt_num2";
+            this.txt_num2.Size = new System.Drawing.Size(56, 20);
+            this.txt_num2.TabIndex = 5;
+            this.txt_num2.Text = "Número 2";
+            // 
+            // txt_num3
+            // 
+            this.txt_num3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txt_num3.Location = new System.Drawing.Point(65, 113);
+            this.txt_num3.Name = "txt_num3";
+            this.txt_num3.Size = new System.Drawing.Size(71, 20);
+            this.txt_num3.TabIndex = 6;
+            this.txt_num3.Text = "Operaciones";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(345, 207);
+            this.Controls.Add(this.txt_num3);
+            this.Controls.Add(this.txt_num2);
+            this.Controls.Add(this.txt_num1);
+            this.Controls.Add(this.comboOperaciones);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ComboBox comboOperaciones;
+        private System.Windows.Forms.TextBox txt_num1;
+        private System.Windows.Forms.TextBox txt_num2;
+        private System.Windows.Forms.TextBox txt_num3;
+    }
+}
+
---
+++ PRÁCTICA 5.2/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRÁCTICA_5._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Calculadora");
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string option = comboOperaciones.Text;
+            string nume1 = txtNum1.Text;
+            string nume2 = txtNum2.Text;
+
+            if (nume1 == "" || nume2 == "" || option == "")
+            {
+                MessageBox.Show("Ingresar los datos: ");
+            }
+            else
+            {
+                double num1 = double.Parse(nume1);
+                double num2 = double.Parse(nume2);
+
+                switch (option)
+                {
+                    case "Suma":
+                        MessageBox.Show("La suma es : " + SUMA(num1,num2));
+                        break;
+
+                    case "Resta":
+                        MessageBox.Show("La resta es : " + RESTA(num1,num2));
+                        break;
+
+                    case "Multiplicación":
+                        MessageBox.Show("La multiplicación es : " + MULTI(num1, num2));
+                        break;
+
+                    case "División":
+                        MessageBox.Show("El división es:  " + DIV(num1, num2));
+                        break;
+                }
+            }
+        }
+
+        // Suma
+        static double SUMA(double num1, double num2) => num1 + num2;
+
+        // Resta
+        static double RESTA(double num1, double num2) => num1 - num2;
+
+        // Multiplicación
+        static double MULTI(double num1, double num2) => num1 * num2;
+
+        //División
+        static double DIV(double num1, double num2) => num1 / num2;
+    }
+}
